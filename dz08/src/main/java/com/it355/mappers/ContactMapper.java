99561d975240929d9e27463b85fa22f86e73bd35
dusanstanojeviccs <dusan.stanojevic.cs@gmail.com>
2016-11-07 15:05:01 +0100

    init

---
+++ dz08/src/main/java/com/it355/mappers/ContactMapper.java
@@ -0,0 +1,20 @@
+package com.it355.mappers;
+
+import com.it355.models.Contact;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ContactMapper implements RowMapper<Contact> {
+
+    public Contact mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Contact contact = new Contact();
+
+        contact.setName(rs.getString("name"));
+        contact.setNumber(rs.getString("number"));
+        contact.setUserId(rs.getLong("user_id"));
+
+        return contact;
+    }
+}
